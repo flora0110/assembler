9e9218e2d7b71dc483601723779fcc1140f94e80
flora <flora90.1.10@gmail.com>
2021-06-16 23:00:40 +0800

    pass 1 ok

--- 4108056029_project.c
+++ 4108056029_project.c
@@ -8,6 +8,8 @@ int main(){
     char *line = NULL;
     FILE *fptr_source;
     FILE *fptr_opcaode;
+    FILE *fptr_location;
+    FILE *fptr_SYMTAB;
     int location_counter;
     int counter = 0;
     char label[100][10];
@@ -15,52 +17,39 @@ int main(){
     char operand[100][10];
     int address[100];
 
-    printf("%s\n",label[0]);
-    //char* source[100];
-    //char *p;
     fptr_source = fopen("source.txt","r");
     fptr_opcaode = fopen("opcode.txt","r");
-    if (fptr_source == NULL || fptr_opcaode==NULL){
+    fptr_location= fopen("location.txt","w");
+    fptr_SYMTAB = fopen("symbol_table.txt","w");
+    if (fptr_source == NULL || fptr_opcaode==NULL ||fptr_location==NULL){
         printf("File Not Found");
         return 0;
     }
     else{
-        //char* p;
         if((read = getline(&line, &len, fptr_source) != -1)){//first line
-            //printf("%d line %s",counter, line);
             char* p_label,*p_opcode,*p_operand;
-            //source[counter] = line;
-            if(line[0]==' '){           //two block(no label)
-                //label[counter][0]='\t';     //label==0
-                strcpy(label[counter],"\t");
+            if(line[0]=='\t'){           //two block(no label)
+                strcpy(label[counter],"\t");//label==0
                 p_opcode = strtok(line,"\t");
             }
             else{                          //three block
                 p_label = strtok(line,"\t");
                 if(p_label){                      //label
-                    //printf("1. %s\n",p_label );
-                    //label[counter]=p_label;
                     strcpy(label[counter], p_label);
-                    //printf("aaa\n");
                     p_opcode=strtok(NULL,"\t");
                 }
             }
             if(p_opcode){                  //opcode
-                //printf("2. %s\n",p_opcode );
-                //opcode[counter]=p_opcode;
                 strcpy(opcode[counter], p_opcode);
                 if(strcmp(p_opcode,"START")==0){            //START
                     p_operand=strtok(NULL,"\t");
                     if(p_operand){                          //has operand
-                        //printf("3. %s\n",p_operand );
-                        //operand[counter] = p_operand;
                         strcpy(operand[counter], p_operand);
                         address[counter]=(int)strtol(p_operand, NULL, 16);
                         location_counter=address[counter];
                     }
                     else{                      //no operand , no address
-                        //operand[counter][0] = '\t';//no operand
-                        strcpy(operand[counter],"\t");
+                        strcpy(operand[counter],"\t");//no operand
                         address[counter] = 0;
                         location_counter = 0;
                     }
@@ -70,81 +59,57 @@ int main(){
                     location_counter=0;
                     p_operand=strtok(NULL,"\t");
                     if(p_operand){                      //has operand
-                        //printf("3. %s\n",p_operand );
-                        //operand[counter] = p_operand;
                         strcpy(operand[counter], p_operand);
                     }
                     else{                      //no operand , no address
-                        //operand[counter][0] = '\t';//no operand
-                        strcpy(operand[counter],"\t");
+                        strcpy(operand[counter],"\t");//no operand
                     }
                 }
             }
             else{                      //no opcode and operand
-                //opcode[counter][0]= '\t';
                 strcpy(opcode[counter],"\t");
-                //operand[counter][0] = '\t';
                 strcpy(operand[counter],"\t");
             }
-            //printf("%d : __________________ %x %s %s %s\n",counter,address[counter],label[counter],opcode[counter],operand[counter]);
-            //printf("operand %s",operand[counter]);
             counter++;
         }
-        //printf("00000000000000000:  %x %s %s %s\n",address[0],label[0],opcode[0],operand[0]);
 
         while ((read = getline(&line, &len, fptr_source) != -1)){//other line
-            //printf("00000000000000000:  %x %s %s %s\n",address[0],label[0],opcode[0],operand[0]);
-            //printf("\n%d line %s",counter, line);
             char* p_label;
             char* p_opcode;
             char* p_operand;
-            //printf("address %p %p %p %p",p_label,p_opcode,p_operand,*label[0]);
-            if(line[0]==' '){           //two block(no label)
-                //label[counter][0]='\t';     //label==0
-                strcpy(label[counter],"\t");
+            if(line[0]=='\t'){           //two block(no label)
+
+                strcpy(label[counter],"\t\0");//label==0
                 p_opcode = strtok(line,"\t");
             }
             else{                          //three block
                 p_label = strtok(line,"\t");
                 if(p_label){                      //label
-                    //printf("1. %s\n",p_label );
-                    //label[counter]=p_label;
                     strcpy(label[counter], p_label);
                     p_opcode=strtok(NULL,"\t");
                 }
             }
             if(p_opcode){                          //opcode
-                //printf("2. %s\n",p_opcode );
-                //opcode[counter]=p_opcode;
-                //strcpy(label[counter], p_label);
                 strcpy(opcode[counter], p_opcode);
                 p_operand=strtok(NULL,"\t");
                 if(p_operand){                      //address
-                    //printf("3. %s\n",p_operand);
-                    //operand[counter] = p_operand;
                     strcpy(operand[counter], p_operand);
                 }
                 else{                      //no address
-                    //operand[counter][0] = '\t';//no operand
-                    strcpy(operand[counter],"\t");
+                    strcpy(operand[counter],"\t");//no operand
                 }
             }
             else{                      //no opcode and operand
-                //opcode[counter][0] = '\t';
                 strcpy(opcode[counter],"\t");
                 strcpy(operand[counter],"\t");
             }
 
             address[counter]=location_counter;
             location_counter=address[counter];
-            //printf("%d : __________________ %x %s %s %s\n",counter,address[counter],label[counter],opcode[counter],operand[counter]);
-            //printf("operand %s",operand[counter]);
             counter++;
             location_counter+=3;
             fflush(stdin);
         }
-        //printf("%x %s %s %s\n",address[0],label[0],opcode[0],operand[0]);
-        printf("\n-----------------------------------------------------------------------\n");
         char *tmp = NULL;
         for(int i=0;i<counter;i++){
             if ((tmp = strstr(opcode[i], "\n"))){
@@ -156,15 +121,18 @@ int main(){
             if ((tmp = strstr(operand[i], "\n"))){
                 *tmp = '\0';
             }
-            //printf("\n%d: \nlabel:%s\n",i,label[i] );
-            //printf("--------------------------\n");
-            //printf("opcode:%s\n",opcode[i]);
-            //printf("--------------------------\n");
-            //printf("operand:%s\n",operand[i]);
-            //printf("--------------------------\n");
-            printf("%X %s %s %s\n",address[i],label[i],opcode[i],operand[i]);
+            fprintf(fptr_location, "%X  %s\t%s\t%s\n",address[i],label[i],opcode[i],operand[i]);
+            printf("%X  %s\t%s\t%s\n",address[i],label[i],opcode[i],operand[i]);
+            if(strcmp(label[i],"\t")!=0 && strcmp(opcode[i],"START")!=0){
+                fprintf(fptr_SYMTAB, "%s\t%X\n",label[i],address[i]);
+                //printf("%s\t%X\n",label[i],address[i]);
+            }
         }
+        //printf("\n", );
         fclose(fptr_source);
+        fclose(fptr_opcaode);
+        fclose(fptr_location);
+        fclose(fptr_SYMTAB);
         if (line){
             free(line);
         }
